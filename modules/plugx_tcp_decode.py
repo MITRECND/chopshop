ce68cd97d732c08d56d7a283ae08ec8c72c0367c
Kyle Creyts <kyle@lastline.com>
2013-12-05 15:47:04 -0800

    cleanup and fixed verbosity

--- modules/plugx_tcp_decode.py
+++ modules/plugx_tcp_decode.py
@@ -49,27 +49,29 @@ def init(module_data):
         # protocol 0 only flags:
         0x0    : "WAITING_FOR_COMMAND?",
         0x1    : "GET_MACHINE_INFO_FLAG",           #returns machine name and identifier
-        0x2    : "CHANGE_LEVEL_FLAG",
+        0x2    : "CHANGE_LEVEL_FLAG (keep-alive?)",
         0x3    : "START_PLUGIN_MGR_FLAG",           #select and enable plugins
         0x5    : "INSTALL_NEW_COPY_FLAG",           #install itself again
         0x6    : "SEND_NEW_SETTINGS_FLAG",          #send bot new settings
         0x7    : "SAVE_SETTINGS_TO_FILE_FLAG",      #save current settings to file
         0x8    : "SEND_PLUGINS_INFO_FLAG",          #send C&C info about plugins
         # protocol 1 only flags:
-        0x1000    : "WAITING_FOR_COMMAND?",
-        0x1001    : "GET_MACHINE_INFO_FLAG",           #returns machine name and identifier
-        0x1002    : "CHANGE_LEVEL_FLAG",
-        0x1003    : "START_PLUGIN_MGR_FLAG",           #select and enable plugins
-        0x1005    : "INSTALL_NEW_COPY_FLAG",           #install itself again
-        0x1006    : "SEND_NEW_SETTINGS_FLAG",          #send bot new settings
-        0x1007    : "SAVE_SETTINGS_TO_FILE_FLAG",      #save current settings to file
-        0x1008    : "SEND_PLUGINS_INFO_FLAG",          #send C&C info about plugins
-        # flags for all protocols:
+        0x1000 : "WAITING_FOR_COMMAND?",
+        0x1001 : "GET_MACHINE_INFO_FLAG",           #returns machine name and identifier
+        0x1002 : "CHANGE_LEVEL_FLAG (keep-alive?)",
+        0x1003 : "START_PLUGIN_MGR_FLAG",           #select and enable plugins
+        0x1005 : "INSTALL_NEW_COPY_FLAG",           #install itself again
+        0x1006 : "SEND_NEW_SETTINGS_FLAG",          #send bot new settings
+        0x1007 : "SAVE_SETTINGS_TO_FILE_FLAG",      #save current settings to file
+        0x1008 : "SEND_PLUGINS_INFO_FLAG",          #send C&C info about plugins
+        # flags for plugins:
+        # Option
         0x2000 : "LOCK_WORKSTATION_FLAG",
         0x2001 : "LOGOFF_FLAG",
         0x2002 : "SYSTEM_REBOOT_FLAG",
         0x2003 : "SYSTEM_SHUTDOWN_FLAG",
         0x2005 : "MESSAGE_BOX_FLAG",
+        # Disk
         0x3000 : "GET_ATTACHED_DISKS_FLAG",
         0x3001 : "SEARCH_DIR_FOR_FILES_FLAG",
         0x3002 : "SEARCH_DIR_RECURSING_FLAG",
@@ -81,24 +83,31 @@ def init(module_data):
         0x300C : "CREATE_DESKTOP_EXEC_FILE_FLAG",
         0x300D : "DO_FILE_OPERATION_FLAG",
         0x300E : "GET_ENV_STRINGS_FLAG",
+        # Screen
         0x4000 : "SCREEN_START_CAP_THREAD_FLAG",
         0x4004 : "SEND_MOUSE_EVENT_FLAG",
         0x4005 : "SEND_KBD_EVENT_FLAG",
         0x4006 : "SEND_CTRL_ALT_DEL_FLAG",
+        # Screen
         0x4100 : "SCREEN_CAPTURE_FLAG",
         0x4101 : "SCREEN_CAPTURE_FRAME_FLAG",
+        # Process
         0x5000 : "ENUM_RUNNING_PROCS_FLAG",
         0x5001 : "ENUM_RUNNING_PROC_MODULES_FLAG",
         0x5002 : "KILL_PROCESS_FLAG",
+        # Service
         0x6000 : "ENUM_SERVICES_FLAG",
         0x6001 : "CHANGE_SERVICE_FLAG",
         0x6002 : "START_SERVICE_FLAG",
         0x6003 : "CONTROL_SERVICE_FLAG",
         0x6004 : "DELETE_SERVICE_FLAG",
+        # Shell
         0x7002 : "START_SHELL_FLAG",
         0x7003 : "SHELL_INTERACT_FLAG",
+        # Telnet
         0x7100 : "START_TELNET_FLAG",
         0x7104 : "TELNET_INTERACT_FLAG",
+        # RegEdit
         0x9000 : "REG_ENUM_KEY_FLAG",
         0x9001 : "REG_OPEN_KEY_FLAG",
         0x9002 : "REG_DEL_KEY_FLAG",
@@ -107,14 +116,19 @@ def init(module_data):
         0x9005 : "REG_CREATE_KEY_WITH_VALUE_FLAG",
         0x9006 : "REG_DEL_VALUE_FLAG",
         0x9007 : "REG_GET_OR_CREATE_VALUE_FLAG",
+        # Nethood
         0xA000 : "NETHOOD_FLAG",
+        # Portmap
         0xB000 : "PORTMAP_FLAG",
+        # SQL
         0xC000 : "SQL_GET_DATA_SOURCE_FLAG",
         0xC001 : "SQL_GET_DRIVER_DESC_FLAG",
         0xC002 : "SQL_EXECUTE_STATEMENT_FLAG",
+        # Netstat
         0xD000 : "TCPSTATE_FLAG",
         0xD001 : "UDPSTATE_FLAG",
         0xD002 : "ADD_TCPSTATE_FLAG",
+        # Keylogger
         0xE000 : "KEYLOGGER_FLAG",
 }
 
@@ -141,7 +155,8 @@ def handleStream(tcp):
         #chop.tsprnt(repr(stage1[8:10]))
     
         if flags & 0x2000000:      #do not decrypt payload separately
-            chop.tsprnt("do not decrypt separately")
+            if tcp.module_data['verbose']:
+                chop.tsprnt("do not decrypt separately")
             size = len(src)
             stage1 = decrypt()
             # consider removing the verbosity filter here if you're seeing the do not decrypt message and not data.
@@ -153,7 +168,8 @@ def handleStream(tcp):
             stage1 = stage1 + decrypt()
         
         if flags & 0x1000000:      #do not decompress payload
-            chop.tsprnt("do not decompress")
+            if tcp.module_data['verbose']:
+                chop.tsprnt("do not decompress")
             return stage1, flags, ''
         else:
             if flags in module_data['flags'].keys():
@@ -237,10 +253,12 @@ def handleStream(tcp):
             chop.tsprnt("c2_side - precrypt - key:%s flag:%s szComp:%s szDeComp:%s" % (repr(hex(unpack("<I", data[0:4])[0])),repr(hex(unpack("<I",data[4:8])[0])),repr(hex(unpack("H",data[8:10])[0])),repr(hex(unpack("H",data[10:12])[0]))))
             chop.tsprnt("c2_side - postcrypt - key:%s flag:%s szComp:%s szDeComp:%s" % (repr(hex(unpack("<I", decrypted[0:4])[0])),repr(hex(unpack("<I",decrypted[4:8])[0])),repr(hex(unpack("H",decrypted[8:10])[0])),repr(hex(unpack("H",decrypted[10:12])[0]))))
         if flags != 0xffff:
-            chop.tsprnt("c2 decrypted header: %s   flag: %s" % (repr(decrypted),hex(flags)))
+            if tcp.module_data['verbose']:
+                chop.tsprnt("c2 decrypted header: %s   flag: %s" % (repr(decrypted),hex(flags)))
             if comp: 
-                chop.tsprnt("printable chars from c2",repr(re.sub(r'[^\w]', '', lznt1.dCompressBuf(comp))),module_data['flags'][flags])
-                chop.tsprnt("full dump of data from c2 %s" % repr(lznt1.dCompressBuf(comp)))
+                chop.tsprnt("printable chars sent to c2",repr(lznt1.dCompressBuf(comp).replace("\x00","")),module_data['flags'][flags])
+                if tcp.module_data['verbose']:
+                    chop.tsprnt("full dump of data sent to c2 %s" % repr(lznt1.dCompressBuf(comp)))
 
         tcp.discard(tcp.server.count_new)
     # handle server system packets
@@ -260,10 +278,12 @@ def handleStream(tcp):
             chop.tsprnt("bot_side - postcrypt: %s %s %s %s" % (repr(hex(unpack("<I", decrypted[0:4])[0])),repr(hex(unpack("<I",decrypted[4:8])[0])),repr(hex(unpack("H",decrypted[8:10])[0])),repr(hex(unpack("H",decrypted[10:12])[0]))))
             chop.tsprnt(comp)
         if flags != 0xffff:
-            chop.tsprnt("bot decrypted header: %s   flag: %s" % (repr(decrypted),hex(flags)))
+            if tcp.module_data['verbose']:
+                chop.tsprnt("bot decrypted header: %s   flag: %s" % (repr(decrypted),hex(flags)))
             if comp: 
-                chop.tsprnt("printable chars from bot",repr(re.sub(r'[^\w]', '', lznt1.dCompressBuf(comp))),module_data['flags'][flags])
-                chop.tsprnt("full dump of data from bot message %s" % repr(lznt1.dCompressBuf(comp)))
+                chop.tsprnt("printable chars sent to bot",repr(lznt1.dCompressBuf(comp).replace("\x00","")),module_data['flags'][flags])
+                if tcp.module_data['verbose']:
+                    chop.tsprnt("full dump of data sent to bot message %s" % repr(lznt1.dCompressBuf(comp)))
         tcp.discard(tcp.client.count_new)
 
     if tcp.stream_data['flag']:
