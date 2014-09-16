2e7a986dd09a76cfc8c73268104ab46f5ca96316
DaS <pdasilva@gmail.com>
2014-09-16 11:33:46 -0400

    Created a documentation file for a quick overview of the module. Updated the netwire chopshop file by adding in a license, and replaced the json code with a python dictionary object

--- modules/netwire.py
+++ modules/netwire.py
@@ -1,11 +1,31 @@
+# Copyright (c) 2014 Palo Alto Networks. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
 """
 Program to decode netwire traffic based on the initial exchange of keys.
 
-author = Unit 42
-copyright = 'Copyright 2014, Palo Alto Networks'
 """
 import binascii
-import json
 import struct
 
 from Crypto.Cipher import AES
@@ -126,11 +146,10 @@ def command_conversion(dest, command, payload, command_list):
     """
     decoded_text = ''
     
-    json_commands = json.loads(command_list)
     command_string = binascii.hexlify(command).upper()
     
-    if json_commands.has_key(command_string):
-        decoded_text = json_commands[command_string]
+    if command_list.has_key(command_string):
+        decoded_text = command_list[command_string]
     else:
         decoded_text = ''
     
@@ -165,8 +184,7 @@ def module_info():
 def init(module_data):
     module_options = { 'proto': [{'tcp': ''}] }
     module_data['password'] = 'Password'
-    module_data['commands'] = """
-{
+    module_data['commands'] = {
     "01": "heartbeat",
     "02": "Socket created",
     "03": "registered",
@@ -244,7 +262,6 @@ def init(module_data):
     "4C": "unset tid for find file get md5",
     "80": "continuation of file download"
 }
-"""
     return module_options
 
 
@@ -329,7 +346,6 @@ def shutdown(module_data):
 
 
 def taste(tcp):
-    " Called when a new stream is detected after setup but before data is received"
     return True
 
 
