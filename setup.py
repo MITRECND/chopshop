aba32e60005f5ab746ee5f984d5788598dbf42d5
Murad Khan <mkhan@mitre.org>
2018-03-16 09:20:59 -0700

    Add setup.py to turn this into a package, version is set, tentatively, to 4.5

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name="chopshop",
+      version="4.5",
+      description="ChopShop",
+      author="MITRE",
+      author_email="",
+      python_requires='>=2.6,<3.0',
+      install_requires=[
+          'future',
+          'pynids',
+          'htpy',
+          'argparse;python_version<"2.7"',
+      ],
+      packages=find_packages(),
+      entry_points={
+        'console_scripts': ['chopshop=chopshop.shop.ChopShop:main',
+                            'chopshop-newmod=chopshop.shop.ChopNewMod:main',
+                            'suture=chopshop.shop.ChopSuture:main']}
+      )
